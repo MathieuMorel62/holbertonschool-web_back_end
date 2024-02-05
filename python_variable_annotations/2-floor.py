3999ae428e0d6f681f9245bbce77f8f17d7ddeb9
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-05 22:00:11 +0000

    Added variable annotation and floor function to calculate the floor value of a given number.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Basic annotations - floor """
+
+
+def floor(n: float) -> float:
+    """Returns the floor value of a given number.
+
+    Args:
+        n (float): The number to calculate the floor value of.
+
+    Returns:
+        int: The floor value of the given number.
+    """
+    return int(n)
