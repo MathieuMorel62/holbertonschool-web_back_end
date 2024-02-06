8a63b8f3285d5f521e4dc2d11267c4587b35bb0a
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-06 16:03:21 +0000

    Updated make_multiply function to improve code readability.

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,13 +6,15 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    Given a float multiplier, return a function that multiplies a float by the multiplier.
+    Given a float multiplier, return a function that multiplies
+    a float by the multiplier.
 
     Args:
         multiplier (float): The float multiplier.
 
     Returns:
-        Callable[[float], float]: The function that multiplies a float by the multiplier.
+        Callable[[float], float]: The function that multiplies
+        a float by the multiplier.
     """
     def multiply(n: float) -> float:
         return n * multiplier
