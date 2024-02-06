08a89fc3d38ad8d54c8167b1302dd13a4ad60d47
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-06 17:29:30 +0000

    Add a type annotation for the 'lst' parameter and the return type of the safe_first_element() function.

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,17 @@ from typing import Sequence, Any, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Return the first element of a sequence if it exists,
+    otherwise return None.
+
+    Args:
+        lst (Sequence[Any]): The sequence from which to retrieve
+        the first element.
+
+    Returns:
+        Optional[Any]: The first element of the sequence, or None if
+        the sequence is empty.
+    """
     if lst:
         return lst[0]
     else:
