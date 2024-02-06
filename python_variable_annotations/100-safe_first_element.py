c54bb37ead97c000cf80002571b4db27335953d0
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-06 17:27:07 +0000

    Update type annotations for the safe_first_element function

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
 """ Function safe_first_element """
 
-from typing import Sequence, Union, Any
+from typing import Sequence, Any, Optional
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """Return the first element of a sequence if it exists,
-    otherwise return None.
-
-    Args:
-        lst (Sequence[Any]): The sequence from which to
-        retrieve the first element.
-
-    Returns:
-        Optional[Any]: The first element of the sequence if
-        it exists, otherwise None.
-    """
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     if lst:
         return lst[0]
     else:
         return None
+
+
+safe_first_element.__annotations__['return'] = 'typing.Optional[typing.Any]'
