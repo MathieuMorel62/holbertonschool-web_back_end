58f118de5b931a3465db7b045d14e9a2ff03cc66
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-07 21:17:01 +0000

    Add asynchronous understanding to generate a list of random numbers.

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Asynchronous Comprehension """
+
+from typing import List
+generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ Asynchronous comprehension that returns a list of random floats. """
+    async_numbers = []
+    async for number in generator():
+        async_numbers.append(number)
+    return async_numbers
