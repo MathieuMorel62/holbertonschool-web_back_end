edf9aae6eed00d8530c84a347770aabdd45c6bcd
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-08 13:09:13 +0000

    Add a get method to retrieve cache values

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -14,12 +14,17 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """ Add an item to the cache """
-        self.cache_data[key] = item
-
         if key is None or item is None:
             return
+        self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_item = next(iter(self.cache_data))
             print("DISCARD: " + first_item)
             del self.cache_data[first_item]
+
+    def get(self, key):
+        """ Get the value associated with the given key from the cache. """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
