8320c136fd1594a2a3e454562f20d3c3eff9880a
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-20 17:29:03 +0000

    Addition of the filter_datum function for obfuscating personal data

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module for implementing a filter_datum function """
+
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ Function that returns the log message obfuscated """
+    for field in fields:
+        pattern = (f"{field}=.*?{separator}")
+        message = re.sub(pattern, (f"{field}={redaction}{separator}"), message)
+    return message
