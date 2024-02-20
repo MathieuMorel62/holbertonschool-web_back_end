bded67abc04adf3558912bf04c27295b11e772e4
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-20 18:34:06 +0000

    Add a RedactingFormatter class to format logs with masked data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,28 @@
 
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            super().format(record),
+            self.SEPARATOR
+        )
 
 
 def filter_datum(
@@ -14,5 +36,6 @@ def filter_datum(
     """ Function that returns the log message obfuscated """
     for field in fields:
         pattern = (f"{field}=.*?{separator}")
-        message = re.sub(pattern, (f"{field}={redaction}{separator}"), message)
+        message = re.sub(pattern, (
+            f" {field}={redaction}{separator}"), message)
     return message
