86d1ab4d62602376d46954a72b9a696afc4d894c
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-20 19:02:34 +0000

    Customised formatting to hide sensitive information in log messages.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,18 +7,32 @@ import logging
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """Custom formatter for redacting sensitive information in log messages."""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        Initialize the RedactingFormatter.
+
+        Args:
+            fields (List[str]): List of fields to be redacted.
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        """
+        Format the log record.
+
+        Args:
+            record (logging.LogRecord): The log record to be formatted.
+
+        Returns:
+            str: The formatted log message.
+        """
         return filter_datum(
             self.fields,
             self.REDACTION,
@@ -33,9 +47,19 @@ def filter_datum(
         message: str,
         separator: str
         ) -> str:
-    """ Function that returns the log message obfuscated """
+    """
+    Redact sensitive information from a log message.
+
+    Args:
+        fields (List[str]): List of fields to be redacted.
+        redaction (str): The string to replace the sensitive information with.
+        message (str): The log message to be redacted.
+        separator (str): The separator used to separate fields in the log mess
+
+    Returns:
+        str: The redacted log message.
+    """
     for field in fields:
-        pattern = (f"{field}=.*?{separator}")
-        message = re.sub(pattern, (
-            f" {field}={redaction}{separator}"), message)
+        pattern = f"{field}=.*?{separator}"
+        message = re.sub(pattern, f" {field}={redaction}", message)
     return message
