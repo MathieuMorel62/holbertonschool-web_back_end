d6b2328e56475429d8f5799e24be305ba88796b6
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-21 19:09:06 +0000

    Fix user search in BasicAuth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -93,10 +93,15 @@ class BasicAuth(Auth):
             return None
         if not isinstance(user_pwd, str):
             return None
-        users = User.search({"email": user_email})
+
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+
         if not users:
             return
-        user = users[0]
-        if not user.is_valid_password(user_pwd):
-            return None
-        return user
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
