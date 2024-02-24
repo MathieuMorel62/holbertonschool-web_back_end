954fd30c2a67138e369c3406cdcec12d98429bd4
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-24 17:00:11 +0000

    Addition of the current_user method to retrieve the current authenticated user.

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Session authentication module """
 
+from models.user import User
 from api.v1.auth.auth import Auth
 import uuid
 
@@ -43,3 +44,21 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        Retrieves the current authenticated user.
+
+        Args:
+            request (Request): The request object (default is None).
+
+        Returns:
+            User: The current authenticated user, or None if not authenticated.
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
