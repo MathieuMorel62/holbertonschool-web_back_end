e5a0f2f02246adecc22b26303fdf5181302e82f3
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-24 20:35:02 +0000

    Session expiry authentication update

--- Session_authentication/api/v1/auth/session_exp_auth.py
+++ Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""""""
+""" Session expiration authentication """
 
 from api.v1.auth.session_auth import SessionAuth
 from datetime import datetime, timedelta
@@ -17,24 +17,42 @@ class SessionExpAuth(SessionAuth):
             pass
 
     def create_session(self, user_id=None):
-        """ """
-        if not user_id:
-            return None
-        session_id = super().create_session(user_id)
-        if not session_id:
-            return None
-        user_id = self.user_id_by_session_id.get(session_id)
-        if not user_id:
-            return None
-        session_dictionary = {
-            "user_id": user_id,
-            "created_at": datetime.now()
-        }
-        self.user_id_by_session_id[session_id] = session_dictionary
-        return session_id
+            """
+            Create a session for the given user ID.
+
+            Args:
+                user_id (str): The ID of the user.
+
+            Returns:
+                str: The session ID if the session was successfully created,
+                    None otherwise.
+            """
+            if not user_id:
+                return None
+            session_id = super().create_session(user_id)
+            if not session_id:
+                return None
+            user_id = self.user_id_by_session_id.get(session_id)
+            if not user_id:
+                return None
+            session_dictionary = {
+                "user_id": user_id,
+                "created_at": datetime.now()
+            }
+            self.user_id_by_session_id[session_id] = session_dictionary
+            return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """ """
+        """
+        Retrieves the user ID associated with a given session ID.
+
+        Args:
+            session_id (str): The session ID.
+
+        Returns:
+            str: The user ID associated with the session ID, or None
+                if the session ID is invalid or expired.
+        """
         if not session_id:
             return None
         session_dictionary = self.user_id_by_session_id.get(session_id)
