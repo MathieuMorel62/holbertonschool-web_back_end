392e514becf43d6d83270304e3a84f4bd009eb23
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-26 19:46:32 +0000

    Update of UUID generation in the authentication service

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -12,9 +12,10 @@ def _hash_password(password: str) -> bytes:
     """ Hashes a password using bcrypt """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def _generate_uuid() -> str:
     """ Generates a UUID """
-    return str(uuid.uuid1())
+    return str(uuid.uuid4())
 
 
 class Auth:
