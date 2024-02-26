35584e593b4011d8921e4e0c9bb9764103fdfe8c
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-26 18:02:48 +0000

    Addition of the Auth class for interaction with the authentication database.

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,8 +2,28 @@
 """ User authentication service """
 
 import bcrypt
+from user import User
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """ Hashes a password using bcrypt """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """ Auth class to interact with the authentication database. """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers a user with the given email and password """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
