c35f98bc6c20706e56319f1c8312f56efe841f71
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-26 16:09:17 +0000

    Add documentation for add_user function

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -26,7 +26,15 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add a user to the database """
+        """Add a new user to the database.
+
+        Args:
+            email (str): The email address of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The newly created User object.
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
