e426fd90a8e9791c800ee3fd27a29174562928f6
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-02-27 16:27:54 +0000

    Add a disconnection route for sessions

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ Main module for the user authentication service """
 
-from flask import Flask, jsonify, request
-from flask import abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -38,6 +37,17 @@ def login():
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ DELETE route for the sessions. """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    else:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
 
 
 if __name__ == "__main__":
