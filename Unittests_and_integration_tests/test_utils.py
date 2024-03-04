01ea7192aaf05c9dd05b211d97cad72c28fa512e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-04 15:38:45 +0000

    Add the test for the memoize decorator to test_utils.py

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -46,3 +46,26 @@ class TestGetJson(unittest.TestCase):
         mock_request.return_value.json.return_value = payload
         self.assertEqual(get_json(url), payload)
         mock_request.assert_called_once_with(url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Class testing memoize decorator"""
+    def test_memoize(self):
+        """ Test memoize decorator """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            test_obj = TestClass()
+            test_obj.a_property
+            test_obj.a_property
+            mock_method.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
