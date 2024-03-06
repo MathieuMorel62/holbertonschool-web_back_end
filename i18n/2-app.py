bcdefb82f151635fab84c605ce584008e9b50d1b
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-06 07:26:57 +0000

    Add localeselector to get_locale function

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -17,6 +17,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
     """ Get locale """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
