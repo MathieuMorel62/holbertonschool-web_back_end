c3909314e07e8193450198197a38f470ff53be74
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-06 09:05:48 +0000

    Fix whitespace in index function*** ***Fix whitespace in index function

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -27,7 +27,7 @@ def get_locale() -> str:
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
-    """ Index"""
+    """ Index """
     return render_template('3-index.html')
 
 
