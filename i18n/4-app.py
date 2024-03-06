1a2a9b205b1715dd9dcade361dd74eff9825a05f
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-06 09:38:47 +0000

    Add locale selector to get_locale function in 4-app.py

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -18,15 +18,13 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
 def get_locale() -> str:
     """ Get locale """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
