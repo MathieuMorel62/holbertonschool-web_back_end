d6500fa8dd81761e19199f73172d4b495b4da5d2
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-19 18:33:30 +0000

    Changes to the request counting and response caching functions in web.py

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -3,44 +3,25 @@
 
 import requests
 import redis
-from typing import Callable
-from functools import wraps
 
 
 redis_client = redis.Redis()
 
 
-def count_requests(method: Callable) -> Callable:
-    """ Decorator to count the number of requests to a URL """
-    @wraps(method)
-    def wrapper(url: str) -> str:
-        """ Wrapper function """
-        count_key = f"count:{url}"
-        redis_client.incr(count_key)
-        return method(url)
-    return wrapper
-
-
-def cache_response(method: Callable) -> Callable:
-    """ Decorator to cache the answer """
-    @wraps(method)
-    def wrapper(url: str) -> str:
-        """ Wrapper function """
-        cache_key = f"cached:{url}"
-        cached = redis_client.get(cache_key)
-
-        if cached:
-            return cached.decode('utf-8')
-
-        response = method(url)
-        redis_client.setex(cache_key, 10, response)
-        return response
-    return wrapper
-
-
-@count_requests
-@cache_response
 def get_page(url: str) -> str:
-    """ Retrieves HTML content from a URL """
+    """ Use the requests module to obtain the HTML content of a particular URL """
+
+    count_key = f"count:{url}"
+    redis_client.incr(count_key)
+
+    cache_key = f"cache:{url}"
+    cached_response = redis_client.get(cache_key)
+
+    if cached_response:
+        return cached_response.decode()
+
     response = requests.get(url)
-    return response.text
+    response_text = response.text
+
+    redis_client.setex(cache_key, 10, response_text)
+    return response_text
