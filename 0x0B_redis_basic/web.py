d0232156baebf5ff5381eb811bc7153c81fa6090
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-19 18:29:45 +0000

    Add decorators to count requests and cache responses

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -3,44 +3,44 @@
 
 import requests
 import redis
+from typing import Callable
 from functools import wraps
-from typing import Callable, Any
 
 
 redis_client = redis.Redis()
 
 
-def count_requests(func: Callable) -> Callable:
-    """ function that will count how many times a request has been made """
-    @wraps(func)
-    def wrapper(url: str, *args, **kwargs) -> Any:
+def count_requests(method: Callable) -> Callable:
+    """ Decorator to count the number of requests to a URL """
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        """ Wrapper function """
         count_key = f"count:{url}"
         redis_client.incr(count_key)
-        return func(url, *args, **kwargs)
-
+        return method(url)
     return wrapper
 
 
-def cache_response(func: Callable) -> Callable:
-    """ function that will cache the response of a request """
-    @wraps(func)
-    def wrapper(url: str, *args, **kwargs) -> Any:
-        cache_key = f"cache:{url}"
-        cached_response = redis_client.get(cache_key)
+def cache_response(method: Callable) -> Callable:
+    """ Decorator to cache the answer """
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        """ Wrapper function """
+        cache_key = f"cached:{url}"
+        cached = redis_client.get(cache_key)
 
-        if cached_response:
-            return cached_response.decode()
+        if cached:
+            return cached.decode('utf-8')
 
-        response = func(url, *args, **kwargs)
+        response = method(url)
         redis_client.setex(cache_key, 10, response)
         return response
-
     return wrapper
 
 
 @count_requests
 @cache_response
 def get_page(url: str) -> str:
-    """ function that will call get_page """
+    """ Retrieves HTML content from a URL """
     response = requests.get(url)
     return response.text
