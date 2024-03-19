0571668196b61db8591f9e36dcbf3c8ad70d5335
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-19 15:48:25 +0000

    Add a decorator to count method calls

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,6 +4,18 @@
 import redis
 import uuid
 from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Decorator to count how many times a method is called """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -13,6 +25,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store the data in Redis using a random key """
         key = str(uuid.uuid4())
