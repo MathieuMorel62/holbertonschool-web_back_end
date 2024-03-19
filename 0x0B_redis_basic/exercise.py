2a1af0660c4e6d4a5ad4169e4547faf3faaf5c36
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-19 15:19:20 +0000

    Add methods for recovering data from Redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,18 +3,34 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
     """ Cache class """
     def __init__(self):
-        """ Constructor """
+        """ Initialize the Cache instance """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Store data in redis """
+        """ Store the data in Redis using a random key """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]) -> \
+            Union[str, bytes, int, float, None]:
+        """ Get the data from Redis """
+        value = self._redis.get(key)
+        if fn and value is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ Get a string from Redis """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """ Get an int from Redis """
+        return self.get(key, fn=int)
