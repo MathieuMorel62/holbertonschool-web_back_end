3e2046e6a30e0e4aea27894828c37e72f644900e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-20 20:48:09 +0000

    Added a pre-drop statement and refined table references in the 'ComputeAverageScoreForUser' stored procedure for improved clarity and reusability.

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,14 @@
--- Create a stored procedure that computes the average score
--- for a user and updates the user's average score in the users table
+-- Create a stored procedure that computes the average score for
+-- a user and updates the user's average score in the users table
 DELIMITER //
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users
     SET average_score = (
       SELECT AVG(score)
       FROM corrections
-      WHERE user_id = user_id
+      WHERE corrections.user_id = user_id
       )
     WHERE id = user_id;
 END //
