ee9f620e3aa0b42012e3d4754721e25859ce1cff
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-20 17:07:51 +0000

    Add a caching and counting function to obtain one page

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -3,26 +3,41 @@
 
 import requests
 import redis
-
+from typing import Callable
+from functools import wraps
 
 redis_client = redis.Redis()
 
+
+def cache_and_count(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs and outputs
+    """
+
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        """
+        Wrapper function
+        """
+        cached_key = f"cached:{url}"
+        count_key = f"count:{url}"
+
+        redis_client.incr(count_key)
+
+        cached_response = redis_client.get(cached_key)
+        if cached_response:
+            return cached_response.decode()
+
+        response = method(url)
+        redis_client.setex(cached_key, 10, response)
+        return response
+    return wrapper
+
+
+@cache_and_count
 def get_page(url: str) -> str:
     """
-    Get a page with caching and counting functionality.
+    Get a page
     """
-    cached_key = f"cached:{url}"
-    count_key = f"count:{url}"
-
-    redis_client.incr(count_key)
-
-    cached_response = redis_client.get(cached_key)
-    if cached_response:
-        return cached_response.decode()
-
     response = requests.get(url)
-    page_content = response.text
-
-    redis_client.setex(cached_key, 10, page_content)
-
-    return page_content
+    return response.text
