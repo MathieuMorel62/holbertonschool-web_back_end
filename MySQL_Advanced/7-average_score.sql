9a67b44752f9e1c631faccc6581190e047b36d93
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-20 20:35:32 +0000

    Refined the 'ComputeAverageScoreForUser' procedure to directly update the user's average score with a streamlined subquery approach

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,14 @@
--- Create a stored procedure that computes the average score for a user and updates the user's average score in the users table
+-- Create a stored procedure that computes the average score
+-- for a user and updates the user's average score in the users table
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10,2);
-
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = (
+      SELECT AVG(score)
+      FROM corrections
+      WHERE user_id = user_id
+      )
     WHERE id = user_id;
 END //
 DELIMITER ;
