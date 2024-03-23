40b3ca93731590b2147ff6000d1c9981a60b29e4
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-23 17:15:27 +0000

    Add comment

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" """
+""" Insert a document in Python """
 
 
 def insert_school(mongo_collection, **kwargs):
-    """"""
+    """ Insert a document in a collection """
     new_document = mongo_collection.insert_one(kwargs)
     return new_document.inserted_id
