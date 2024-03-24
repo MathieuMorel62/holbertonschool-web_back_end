a6d0667ca779b6b69abc7b8f45f6ca3fd54d4fc9
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-24 12:56:01 +0000

    Update to the handleProfileSignup function to remove the firstName, lastName and fileName arguments

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,8 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
+    signUpUser(),
+    uploadPhoto(),
   ]).then((results) => results.map((result) => ({
     status: result.status,
     value: result.value || result.reason,
