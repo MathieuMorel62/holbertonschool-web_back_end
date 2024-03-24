8507b9f78e0da5cf1b0b62f696a87fccc28c6996
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-24 19:46:15 +0000

    Added endpoint load management with WeakMap in queryAPI, throwing an error when the number of queries exceeds the allowed threshold.

---
+++ ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(endpoint, count + 1);
+}
+
+export { weakMap, queryAPI };
