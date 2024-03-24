d0d64d5f6d3a00df23f089aa78671aca8c3c6d9d
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-24 19:19:28 +0000

    "Implemented cleanSet to filter and format the elements of a Set starting with a specified string, returning the concatenated result with dashes

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+
+  return Array.from(set)
+    .filter((item) => item && item.startsWith(startString))
+    .map((item) => item.slice(startString.length))
+    .join('-');
+}
+
+export default cleanSet;
