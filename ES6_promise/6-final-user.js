3d0022407014e303460fb11000476e744ffb28be
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-24 13:13:33 +0000

    Refactored handleProfileSignup to improve code clarity when managing Promise.allSettled results.

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,8 +5,13 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.value || result.reason,
-  })));
+  ]).then((results) => results.map((result) => {
+    let value;
+    if (result.status === 'fulfilled') {
+      value = result.value;
+    } else {
+      value = result.reason;
+    }
+    return { status: result.status, value };
+  }));
 }
