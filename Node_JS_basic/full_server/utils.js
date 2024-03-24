9d70e904bb55f2cd0f97e5947af59ce4a1d576c0
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-24 22:26:16 +0000

    code refactoring for respect linter

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -6,20 +6,10 @@ export default function readDatabase(path) {
       if (err) {
         reject(Error('Cannot load the database'));
       } else {
-        const [headerLine, ...lines] = data
-          .split('\n')
-          .filter((line) => line.length > 0);
+        const [headerLine, ...lines] = data.split('\n').filter((line) => line.length > 0);
         const headers = headerLine.split(',');
 
-        const listObj = lines.map((line) =>
-          line
-            .split(',')
-            .reduce(
-              (object, currentValue, index) =>
-                Object.assign(object, { [headers[index]]: currentValue }),
-              {}
-            )
-        );
+        const listObj = lines.map((line) => line.split(',').reduce((object, currentValue, index) => Object.assign(object, { [headers[index]]: currentValue }), {}));
 
         const groupByField = listObj.reduce((res, currentValue) => {
           res[currentValue.field] = res[currentValue.field] || [];
