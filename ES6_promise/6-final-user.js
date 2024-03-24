7631b4f5369cbb12754b47ef2ac1e2ca06c08ebe
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-24 13:15:08 +0000

    Refactoring of the handleProfileSignup function to use a more concise syntax

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,13 +5,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => {
-    let value;
-    if (result.status === 'fulfilled') {
-      value = result.value;
-    } else {
-      value = result.reason;
-    }
-    return { status: result.status, value };
-  }));
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
