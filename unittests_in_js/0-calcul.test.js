80df134068932fac48326ae33746119e5c5ad78f
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-25 16:11:01 +0000

    Add unit tests for the calculateNumber function

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,16 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
-  it('Rounds 1 and 3 to 4', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+  it('Test sum of integers', () => {
+    assert.equal(calculateNumber(1, 3), 4);
   }),
-  it('Rounds 1 and 3.7 to 5', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it('Test sum with floating point number', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
   }),
-  it('Rounds 1.2 and 3.7 to 5', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it('Test sum with two floating point numbers', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   }),
-  it('Rounds 1.5 and 3.7 to 6', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('Test sum of rounded numbers', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  }),
+  it('Test sum of negative numbers', () => {
+    assert.equal(calculateNumber(-1, -3), -4);
   })
 });
