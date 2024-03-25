8d713669c24c178313d5981d96607a37dca3f2ce
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-25 16:21:27 +0000

    Add validation for invalid entries in the calculateNumber() function

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,5 +1,9 @@
 // Function that returns the sum of 2 numbers and rounds them
 function calculateNumber(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Invalid input');
+  }
+
   return Math.round(a) + Math.round(b);
 }
 
