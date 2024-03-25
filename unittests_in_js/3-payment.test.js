649be3ee68d558c5710b8c5bf16066f4f9cc671e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-25 19:13:24 +0000

    Added tests in 3-payment.test.js to verify sendPaymentRequestToApi functionality with spies.

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('Validates the usage of the Utils.calculateNumber function', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spy.returnValues[0]).to.equal(120);
+
+    spy.restore();
+  });
+});
