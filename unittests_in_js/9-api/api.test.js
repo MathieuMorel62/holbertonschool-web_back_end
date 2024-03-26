ad5a34e084722dbd1d5b73ed7846ced992f75ddf
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-26 12:05:10 +0000

    API tests updated to use the Chai library and the request function

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,23 +1,18 @@
-const chai = require("chai");
-const request = require("request");
+const chai = require('chai');
+const request = require('request');
 const expect = chai.expect;
 
-describe("GET /cart/:id", () => {
-  it("Should return payment methods for numeric id", (done) => {
-    chai.request(app)
-    .get('/cart/123')
-    .end((err, res) => {
-      res.should.have.status(200);
-      res.text.should.equal('Payment methods for cart 123');
+describe('GET /cart/:id', () => {
+  it('Should return payment methods for numeric id', (done) => {
+    request.get('http://localhost:7865/cart/123', (err, res, body) => {
+      expect(body).to.equal('Payment methods for cart 123');
       done();
     });
   });
 
-  it("Should return 404 for non-numeric id", (done) => {
-    chai.request(app)
-    .get('/cart/abc')
-    .end((err, res) => {
-      res.should.have.status(404);
+  it('Should return 404 for non-numeric id', (done) => {
+    request.get('http://localhost:7865/cart/abc', (err, res, body) => {
+      expect(response.statusCode).to.equal(404);
       done();
     });
   });
