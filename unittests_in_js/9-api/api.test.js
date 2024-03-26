f2e210e751b98a6979d7dc52dc3a2fd517904ac9
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-03-26 12:12:21 +0000

    Added tests for the 'GET /cart/:id' endpoint, checking the status code and the body of the response.

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -3,6 +3,20 @@ const request = require('request');
 const expect = chai.expect;
 
 describe('GET /cart/:id', () => {
+  it("should return correct status code", (done) => {
+    request("http://localhost:7865", (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it("should return correct result", (done) => {
+    request("http://localhost:7865", (error, response, body) => {
+      expect(body).to.equal("Welcome to the payment system");
+      done();
+    });
+  });
+
   it('Should return payment methods for numeric id', (done) => {
     request.get('http://localhost:7865/cart/123', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
