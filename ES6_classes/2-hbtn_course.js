a783d311e9ae8fa0f52468415e9384ed00907062
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-10-16 12:45:59 +0200

    Respect semistandard

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,5 @@
 export default class HolbertonCourse {
-  constructor(name, length, students) {
+  constructor (name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
@@ -16,34 +16,34 @@ export default class HolbertonCourse {
   }
 
   // Getters
-  get name() {
+  get name () {
     return this._name;
   }
 
-  get length() {
+  get length () {
     return this._length;
   }
 
-  get students() {
+  get students () {
     return this._students;
   }
 
   // Setters
-  set name(newName) {
+  set name (newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
 
-  set length(newLength) {
+  set length (newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = newLength;
   }
 
-  set students(newStudents) {
+  set students (newStudents) {
     if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
